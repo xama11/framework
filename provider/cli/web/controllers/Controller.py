7c199db2c1334aed091d19c69a0960091ff49fab
silvaleal <lealsilva.ctt@outlook.com>
2025-12-30 18:31:12 -0300

    dashboard: adjusts

--- provider/cli/web/controllers/Controller.py
+++ provider/cli/web/controllers/Controller.py
@@ -4,10 +4,11 @@ class Controller:
     def __init__(self, request):
         self.request = request
         self.decorators = []
+        self.commands = []
         
         self.setValues()
         
         
     def setValues(self):
         self.decorators = [decorator for decorator in os.listdir('application/decorators') if decorator.endswith('.py') and not '_' in decorator]
-        
+        self.commands = [file for file in os.listdir('application/cogs/') if file.endswith('.py')]
--- provider/cli/web/controllers/HomeController.py
+++ provider/cli/web/controllers/HomeController.py
@@ -10,14 +10,14 @@ class HomeController(Controller):
         super().__init__(request)
     
     def view(self):
-        commands = len([file for file in os.listdir('application/cogs/') if file.endswith('.py')])
+        
         
         migrations = [migration for migration in MigrationsModel().get().limit(10).all() if not migration[1][0]=='0']
         terminals = TerminalsModel().get().limit(10).orderBy('id').all()
 
         return render_template(
             'home.html',
-            commands=commands,
+            commands=self.commands,
             migrations=migrations,
             terminals=terminals
         )
